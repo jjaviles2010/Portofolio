9b0496aa7f7e85f5f70f873bf1e440d544410826
Jose Javier Aviles Gomez <jose.aviles@nibspe.com.br>
2019-10-13 23:59:16 -0300

    Implementando logica para mostrar minhas informacoes

--- js/load.js
+++ js/load.js
@@ -22,17 +22,18 @@ function load() {
 function showBoxInfo(element) {
     const boxToShow = element.getAttribute('data-box');
 
-    const btns = document.querySelectorAll('.btn-infos');
+    const btns = document.querySelectorAll('.btn__active');
     const descriptions = document.querySelectorAll('.box__descriptions');
     
     btns.forEach((btn) => {
-        btn.className = btn.className.replace('btn__active', '');
+        btn.className = btn.className.replace('btn__active', 'btn__inactive');
     });
     
     descriptions.forEach((box) => {
         box.className = box.className.replace('box__active', '');
     });
 
+    element.className = element.className + ' btn__active';
     const elementBox = document.querySelector(`.box__${boxToShow}`);
     elementBox.className = elementBox.className + ' box__active';
 }
@@ -63,6 +64,7 @@ function enviar() {
     const subject = document.querySelector('.subject__field').value;
     const email = document.querySelector('.email__field').value;
     const message = document.querySelector('.message__field').value;
+
 
     const msg = {
         from: "leandro.faria@assessoria.app",
